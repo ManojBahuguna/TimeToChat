b3e0173d6da9791ceb35ae50c8fa1f8cd085250e
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2017-06-10 16:42:39 +0530

    setup mongoose database

---
+++ configs/database.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    localDbUrl : 'mongodb://127.0.0.1/mono_devs',
+    mlabDbUrl : 'mongodb://mono:Jonam1996@ds058369.mlab.com:58369/mono_devs',
+    password : 'Jonam1996'
+}
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+//Config files imports
 const serverConfig = require('./configs/server.config');
+const dbConfig = require('./configs/database.config');
 
+//Other imports
 const express = require('express');
 const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
 
-app.use(express.static('public'));
+//Setup Mongoose
+mongoose.connect(dbConfig.localDbUrl);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database!');
+});
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
 
-app.listen(serverConfig.port, function(){
+
+//Express middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Start Express Server
+app.listen(serverConfig.port, ()=>{
     console.log('Server started at port : ' + serverConfig.port);
 });
